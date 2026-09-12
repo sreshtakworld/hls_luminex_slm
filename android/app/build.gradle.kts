09c20481bdee27abb78cfd60f3b2b7e79b511997
sreshtakworld <sreshtakworld@gmail.com>
2026-09-12 22:50:36 +0530

    Add ProGuard configuration for release build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -29,6 +29,11 @@ android {
     buildTypes {
         release {
             signingConfig = signingConfigs.getByName("debug")
+
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
 }
